34ba11b3537762deea94317da9d06b8360e6d034
TaylorZhang <taylorkunzhang@gmail.com>
2021-04-06 17:44:30 +0800

    Optimize bucket sort

--- Sort/Kotlin/src/bucketSort/BucketSort.kt
+++ Sort/Kotlin/src/bucketSort/BucketSort.kt
@@ -1,29 +1,31 @@
 package bucketSort
 
 fun bucketSort(arr: IntArray) {
-    if (arr.isEmpty()) return
+    if (arr.size < 2) return
     var min = arr[0]
     var max = arr[0]
-    for (value in arr) {
-        if (value < min) {
-            min = value
-        } else if (value > max) {
-            max = value
+    var num: Int
+    for (i in 1 until arr.size) {
+        num = arr[i]
+        if (num < min) {
+            min = num
+        } else if (num > max) {
+            max = num
         }
     }
 
     val bucketNum = (max - min) / arr.size + 1
-    val buckList = ArrayList<ArrayList<Int>>()
+    val bucketList = ArrayList<ArrayList<Int>>()
     for (i in 0 until bucketNum) {
-        buckList.add(ArrayList())
+        bucketList.add(ArrayList())
     }
     for (i in arr.indices) {
-        val num = (arr[i] - min) / arr.size
-        buckList[num].add(arr[i])
+        num = (arr[i] - min) / arr.size
+        bucketList[num].add(arr[i])
     }
 
     var index = 0
-    for (list in buckList) {
+    for (list in bucketList) {
         list.sort()
         for (k in list) {
             arr[index++] = k
